497a2350ed3ca1581484723b2d6d1cf130e18dbf
Luiz Carraro <lacarraro@gmail.com>
2018-03-14 19:20:12 -0300

    adiciona exemplos de ember

--- exemplo_react/src/App.js
+++ exemplo_react/src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let user = {
+  name: 'Luiz'
+}
+function hello(user) {
+  return "Olá " + user.name;
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{hello(user)}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
